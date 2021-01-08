df3e100901b58b5f24dd3c20c9e0745b6337f3af
fish <1161602814@qq.com>
2021-01-08 10:57:28 +0800

    feat(tests): 添加的单元测试文件

---
+++ tests/adminAction/IndexActionTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace tests\adminAction;
+
+use framework\Container;
+use PHPUnit\Framework\TestCase;
+
+class IndexActionTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
